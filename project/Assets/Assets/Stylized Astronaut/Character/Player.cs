b34430ec5535dedff0478c275b6b55c247c0cacd
alabelle127 <alabelle@umass.edu>
2022-12-17 12:52:45 -0500

    Added start menu and astronaut guy from project 1 as an optional asset. First person sounds good if you already have it working well. I also added mesh colliders on the states (scaled) and non-scaled

--- project/Assets/Assets/Stylized Astronaut/Character/Player.cs
+++ project/Assets/Assets/Stylized Astronaut/Character/Player.cs
@@ -7,14 +7,19 @@ public class Player : MonoBehaviour
     private Animator anim;
     private CharacterController controller;
 
-    public float speed = 600.0f;
+    public float speed = 50.0f;
     public float turnSpeed = 400.0f;
     private Vector3 moveDirection = Vector3.zero;
     public float gravity = 20.0f;
-		public float jump_force = 20.0f;
+		public float jump_force = 3.0f;
     // public TerrainCollider TerrainCollider;
     private Bounds bounds;
     private Vector3 startPosition;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private bool canJump;
+    public float distToGround = 1f;
 
     void Start()
     {
@@ -22,6 +27,8 @@ public class Player : MonoBehaviour
         anim = gameObject.GetComponentInChildren<Animator>();
         // bounds = TerrainCollider.bounds;
         startPosition = transform.position;
+        camForward = mainCamera.GetComponent<Transform>().forward;
+        canJump = true;
     }
 
     // private bool isCharacterTryingToBreakThrough() {
@@ -42,7 +49,11 @@ public class Player : MonoBehaviour
         //   transform.position = startPosition;
         //   controller.enabled = true;
         // }
-        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow)
+          || Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow)
+          || Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow)
+          || Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow)
+        )
         {
             anim.SetInteger("AnimationPar", 1);
         }
@@ -51,17 +62,32 @@ public class Player : MonoBehaviour
             anim.SetInteger("AnimationPar", 0);
         }
 
-        if (controller.isGrounded) {
-					moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-					if(Input.GetKeyDown("space")) {
-						moveDirection.y += jump_force;
-					}
-				}
+        moveDirection = ( Input.GetAxis("Vertical") + Input.GetAxis("Horizontal") ) * camForward * speed;
+        // moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical")) * camForward * speed;
+
+        canJump = Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f);
+
+        if(canJump) {
+          if(Input.GetKeyDown("space")) {
+            canJump = false;
+            moveDirection.y += jump_force;
+          }
+        }
+
+
+
+        // if (controller.isGrounded) {
+				// 	moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+				// 	if(Input.GetKeyDown("space")) {
+				// 		moveDirection.y += jump_force;
+				// 	}
+				// }
 
 				moveDirection.y -= gravity * Time.deltaTime;		
 
-        float turn = Input.GetAxis("Horizontal");
-        transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+        // float turn = Input.GetAxis("Horizontal");
+        // transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+        // transform.rotation = Quaternion.LookRotation(moveDirection.normalized);
         controller.Move(moveDirection * Time.deltaTime);
     }
 }
--- project/Assets/Assets/Stylized Astronaut/Character/ThirdPersonCamera.cs
+++ project/Assets/Assets/Stylized Astronaut/Character/ThirdPersonCamera.cs
@@ -9,7 +9,16 @@ public class ThirdPersonCamera : MonoBehaviour
 
     public Transform lookAt;
     public Transform camTransform;
-    public float distance = 5.0f;
+    public float distance = 2.5f;
+
+
+    // For zooming in/out with scrollwheel
+    public Transform parentObject;
+    public float zoomLevel;
+    public float maxZoom = 30.0f;
+    public float speed = 30.0f;
+    public float sensitivity = 1.0f;
+    private float zoomPosition;
 
     private float currentX = 0.0f;
     private float currentY = 45.0f;
@@ -28,6 +37,10 @@ public class ThirdPersonCamera : MonoBehaviour
             currentY -= Input.GetAxis("Mouse Y")*2;
         }
 
+        // distance += Input.mouseScrollDelta.y * sensitivity;
+        // distance = Mathf.Clamp(distance, 0, maxZoom);
+        // zoomPosition = Mathf.MoveTowards(zoomPosition, distance, speed * Time.deltaTime);
+        // transform.position = parentObject.position + (transform.forward * zoomPosition);
 
         currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
     }
